81f32abe69d4b6c62fba783e63e6f13be4822f31
Chris Wood <chris.wood@skyscanner.net>
2017-02-02 17:26:46 +0000

    Refactor with functions

--- section2/ex6.py
+++ section2/ex6.py
@@ -5,16 +5,34 @@ smallest_parcel = {}
 biggest = 0
 smallest = None
 
+def update_biggest(size, destination, length, width, height):
+    biggest_parcel['destination'] = destination
+    biggest_parcel['length'] = length
+    biggest_parcel['width'] = width
+    biggest_parcel['height'] = height
+
+def update_smallest(size, destination, length, width, height):
+    smallest_parcel['destination'] = destination
+    smallest_parcel['length'] = length
+    smallest_parcel['width'] = width
+    smallest_parcel['height'] = height
+
+def print_biggest():
+    print "Biggest"
+    for key in biggest_parcel:
+        print "%s: %s" % (key, biggest_parcel[key])
+
+def print_smallest():
+    print "Smallest"
+    for key in smallest_parcel:
+        print "%s: %s" % (key, smallest_parcel[key])
+
 while True:
     print 'Enter a parcel'
     destination = raw_input('Destination: ')
     if destination == '':
-        print "Biggest"
-        for key in biggest_parcel:
-            print "%s: %s" % (key, biggest_parcel[key])
-        print "Smallest"
-        for key in smallest_parcel:
-            print "%s: %s" % (key, smallest_parcel[key])
+        print_biggest()
+        print_smallest()
         break
     dimensions = raw_input('Dimensions (L W H): ')
     length = int(dimensions.split(' ')[0])
@@ -23,16 +41,10 @@ while True:
     size = length * width * height
     if size > biggest:
         biggest = size
-        biggest_parcel['destination'] = destination
-        biggest_parcel['length'] = length
-        biggest_parcel['width'] = width
-        biggest_parcel['height'] = height
+        update_biggest(size, destination, length, width, height)
     if smallest is None or size < smallest:
         smallest = size
-        smallest_parcel['destination'] = destination
-        smallest_parcel['length'] = length
-        smallest_parcel['width'] = width
-        smallest_parcel['height'] = height
+        update_smallest(size, destination, length, width, height)
 
 
 
