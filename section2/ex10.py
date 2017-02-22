adef31fa7284146b6cb9ad81b5c8726775d92e36
Chris Wood <chris.wood@skyscanner.net>
2017-02-22 20:47:43 +0000

    Make column order dynamic
    Also hash rows

--- section2/ex10.py
+++ section2/ex10.py
@@ -3,22 +3,40 @@
 import csv
 import sys
 
+
 with open(sys.argv[1], 'r') as f:
     values = csv.reader(f)
     respondents = {}
+    columns = []
+    row_count = 1
     for row in values:
-        if row[0] == 'id':
+        if row_count == 1:
+            columns = row
+            row_count = row_count + 1
             continue
-        email = row[1].lstrip()
-        respondents[email] = row
+        column_count = 0
+        for column in columns:
+            column = column.strip(' ')
+            if column == 'email':
+                respondents[row[column_count]] = row
+            column_count = column_count + 1
 
 live_in_scotland = 0
 yes_to_q2 = 0
+respondents_hashed = {}
 for k,v in respondents.iteritems():
-    if v[2] == ' scotland':
-        live_in_scotland = live_in_scotland + 1
-    if v[4] == ' 1':
-        yes_to_q2 = yes_to_q2 + 1
+    stripped_key = k.strip(' ')
+    respondents_hashed[stripped_key] = {}
+    col_count = len(columns) - 1
+    while col_count > 0:
+        stripped_column = columns[col_count].strip(' ')
+        stripped_value = v[col_count].strip(' ')
+        respondents_hashed[stripped_key][stripped_column] = stripped_value
+        col_count = col_count - 1
 
 print "%i people live in scotland" % live_in_scotland
 print "%i people answered yes to q2" % yes_to_q2
+
+for k,v in respondents_hashed.iteritems():
+    print k
+    print v
